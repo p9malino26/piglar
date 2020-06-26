99c00a50425025bcff65ea26a4a91a28b66fb2be
p9malino26 <pm3g19@soton.ac.uk>
2020-06-26 12:01:23 +0100

    Initial commit for file. Still untested.

---
+++ src/generator/generator.cpp
@@ -0,0 +1,23 @@
+#include "../RoadMap.h"
+#include "../GLMIncludes.h"
+#include "../Random.h"
+
+#define CONSTANT(EXPR) \
+static constexpr EXPR;
+
+void generateTree(RoadMap* roadmap, const glm::vec2i& startpos)
+{
+    assert(!roadmap->isPositionOutside(startpos));
+    CONSTANT(int MIN_MAINROAD_LENGTH = 4)
+    CONSTANT(int MAX_MAINROAD_LENGTH = 15)
+    int mainRoadLength = Random::randInt(MIN_MAINROAD_LENGTH, MAX_MAINROAD_LENGTH);
+    glm::vec2i endPos = startpos + glm::vec2i(0, mainRoadLength);
+    assert(!roadmap->isPositionOutside(endPos));
+
+    for (glm::vec2i pos = startpos; ; pos.y++)
+    {
+        roadmap->setFieldState(pos, true);
+        if (startpos == endPos)
+            break;
+    }
+}
