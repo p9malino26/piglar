d7c54bf56878c96ca0c975b49e1091db8c96fab5
p9malino26 <pm3g19@soton.ac.uk>
2020-07-05 10:18:36 +0100

    Changed roadmap passing from pointer to reference

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -2,12 +2,12 @@
 
 #include "Input.h"
 
-void generateTree(RoadMap* roadmap, const glm::vec2i& startpos);
+void generateTree(RoadMap& roadmap, const glm::vec2i& startpos);
 
 Scene::Scene()
     :roadMap(width, height), roadGenerator(&roadMap)
 {
-    generateTree(&roadMap, glm::vec2i(25, 5));
+    generateTree(roadMap, glm::vec2i(25, 5));
 }
 
 void Scene::update()
--- src/generator/generator.cpp
+++ src/generator/generator.cpp
@@ -1,4 +1,5 @@
-#include<iostream>
+#include <iostream>
+#include <random>
 
 #include "../RoadMap.h"
 #include "../GLMIncludes.h"
@@ -7,19 +8,20 @@
 #define CONSTANT(EXPR) \
 static constexpr EXPR;
 
-void generateTree(RoadMap* roadmap, const glm::vec2i& startpos)
+void generateTree(RoadMap& roadmap, const glm::vec2i& startpos)
 {
-    assert(!roadmap->isPositionOutside(startpos));
+    assert(!roadmap.isPositionOutside(startpos));
     CONSTANT(int MIN_MAINROAD_LENGTH = 4)
     CONSTANT(int MAX_MAINROAD_LENGTH = 15)
+
     int mainRoadLength = Random::randInt(MIN_MAINROAD_LENGTH, MAX_MAINROAD_LENGTH);
     glm::vec2i endPos = startpos + glm::vec2i(0, mainRoadLength);
-    assert(!roadmap->isPositionOutside(endPos));
+    assert(!roadmap.isPositionOutside(endPos));
 
     for (glm::vec2i pos = startpos; ; pos.y++)
     {
         std::cout << "Pos: " << pos << std::endl;
-        roadmap->setFieldState(pos, true);
+        roadmap.setFieldState(pos, true);
         if (pos.y == endPos.y)
             break;
     }
