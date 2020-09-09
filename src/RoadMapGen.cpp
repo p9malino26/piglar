04d2ad37059a1bc1fa1edac7c7087ea748830d9f
p9malino26 <pm3g19@soton.ac.uk>
2020-09-09 14:42:22 +0100

    This is how the steps of generating and placing will work.

--- src/RoadMapGen.cpp
+++ src/RoadMapGen.cpp
@@ -18,14 +18,17 @@ RoadMapGen::RoadMapGen(RoadMap* roadMap)
 
 }
 
+glm::vec2i toFinalPlacePos(const Generator::ClosestPointInfo& closestPointInfo, const glm::vec2i& dims);
+
 void RoadMapGen::generate()
 {
-    Generator::TreeGenParams params;
+    using namespace Generator;
+    TreeGenParams params;
 
     params.mainRoadLengthRange = {10,15};
     params.branchRoadLengthRange = {10, 14};
     params.branchStepRange = {2, 3};
-    Generator::TreeGenerator treeGen(params);
+    TreeGenerator treeGen(params);
 
     auto getStartPos = [](const glm::vec2i& roadMapsize, const glm::vec2i& dims) -> glm::vec2i {
         glm::vec2i offset = dims / 2;
@@ -41,33 +44,17 @@ void RoadMapGen::generate()
     //record
     rpr->placeRectangle(placePos, dims);
 
-
-    auto closestPointInfo = rpr.getClosestPoint();
-
-
-    //new code
     /*
-    bool orientation = 0;
+    //get closest point
+    auto closestPointInfo = rpr->getClosestPoint();
 
-    const int N_ITERATIONS = 10;
-    RANGE_FOR(i, 0, N_ITERATIONS)
-    {
-        //generate and get dimensions
-        glm::vec2i dims = treeGen.generate();
-        //get closest point
-        auto closestPointInfo = rpr.getClosestPoint();
+    //generate again
+    dims = treeGen.generate();
+    rpr->placeRectangle(toFinalPlacePos(closestPointInfo, dims), dims);*/
+}
 
-
-        //find out if there is space
-        bool canBeHorizontal =
-
-        treeGen.writeTo(*roadMap, placePos, orientation);
-        rpr.placeRectangle(placePos, dims);
-
-
-
-    }
-    */
+glm::vec2i toFinalPlacePos(const Generator::ClosestPointInfo& closestPointInfo, const glm::vec2i& dims)
+{
 }
 
 RoadMapGen::~RoadMapGen()
